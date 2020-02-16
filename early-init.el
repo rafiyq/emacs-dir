e29fc31b2eb788cebb2caa52fe05c0ee3da11157
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-02-16 12:05:44 +0700

    Add emacs built-in feature and packages.

--- early-init.el
+++ early-init.el
@@ -4,20 +4,19 @@
 (defvar init-gc-cons-percentage gc-cons-percentage)
 
 (setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
+  gc-cons-percentage 0.6)
 
 (add-hook 'emacs-startup-hook
-	  (lambda ()
-	    (setq init-gc-cons-threshold 
-		  init-gc-cons-percentage)))
+          (lambda ()
+            (setq init-gc-cons-threshold
+                  init-gc-cons-percentage)))
 
 (setq package-enable-at-startup nil)
 (advice-add #'package--ensure-init-file :override #'ignore)
 
-(setq tool-bar-mode nil
-      menu-bar-mode nil)
-(when (fboundp 'set-scroll-bar-mode)
-  (set-scroll-bar-mode nil))
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 
 (setq frame-inhibit-implied-resize t)
 
--- init.el
+++ init.el
@@ -8,18 +8,6 @@
   (setq file-name-handler-alist initial-file-name-handler-alist))
 (add-hook 'emacs-startup-hook #'reset-file-handler-alist-h)
 
-(require 'cl-lib)
-(require 'map)
-(require 'subr-x)
-
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message user-login-name
-      inhibit-default-init t
-      initial-major-mode 'fundamental-mode
-      initial-scratch-message nil)
-
-(fset #'display-startup-echo-area-message #'ignore)
-
 (defconst is-mac     (eq system-type 'darwin))
 (defconst is-linux   (eq system-type 'gnu/linux))
 (defconst is-windows (memq system-type '(cygwin windows-nt ms-dos)))
@@ -68,37 +56,121 @@
      :straight nil
      ,@args))
 
+(use-package delight
+    :demand t)
+
 (use-feature straight-x
   :commands (straight-x-fetch-all))
 
-(setq enable-recursive-minibuffers t)
-
-(setq echo-keystrokes 0.02)
-
-(setq resize-mini-windows 'grow-only
-      max-mini-window-height 0.15)
-
-(fset #'yes-or-no-p #'y-or-n-p)
-
-(setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-(setq confirm-kill-emacs #'y-or-n-p)
-
-(setq uniquify-buffer-name-style 'forward)
+(straight-use-package
+ '(org :host github :repo "emacs-straight/org-mode" :local-repo "org"))
 
 (setq window-resize-pixelwise t
   frame-resize-pixelwise t)
-
-(setq window-divider-default-places t
-  window-divider-default-bottom-width 1
-  window-divider-default-right-width 1)
 
 (setq split-width-threshold 160
   split-height-threshold nil)
 
 (setq indicate-buffer-boundaries nil
-  indicate-empty-lines nil)
+      indicate-empty-lines t)
+
+(use-feature winner
+  :demand t
+  :config
+  (winner-mode +1))
+
+(setq window-divider-default-places t
+      window-divider-default-bottom-width 1
+      window-divider-default-right-width 1)
+(add-hook 'window-setup-hook #'window-divider-mode)
+
+(use-feature uniquify
+  :demand t
+  :init
+  (setq uniquify-buffer-name-style 'forward))
+
+(column-number-mode +1)
+
+(setq enable-recursive-minibuffers t
+      resize-mini-windows 'grow-only
+      max-mini-window-height 0.15
+      minibuffer-prompt-properties
+      '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+(use-feature savehist
+  :demand t
+  :init
+  (setq savehist-file (concat cache-dir "savehist")
+        savehist-save-minibuffer-history t
+        savehist-autosave-interval nil
+        savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
+  :config
+  (savehist-mode +1))
+
+(setq use-file-dialog nil
+      use-dialog-box nil
+      inhibit-splash-screen t
+      initial-scratch-message nil)
+
+(use-feature tooltip
+  :init
+  (when (bound-and-true-p tooltip-mode)
+    (tooltip-mode -1))
+  (when is-linux
+    (setq x-gtk-use-system-tooltips nil)))
+
+(use-feature emacs
+  :init
+  (setq x-underline-at-descent-line t
+        underline-minimum-offset 1)
+
+  (set-face-attribute 'default nil :height 100)
+  (add-to-list 'default-frame-alist `(font . "Fira Mono"))
+  (set-face-attribute 'fixed-pitch nil :family 'unspecified))
+
+(use-package emacs-color-theme-solarized
+  :straight (:host github :repo "sellout/emacs-color-theme-solarized")
+  :init
+  (setq solarized-termcolor 256
+        solarized-broken-srgb t
+        solarized-contrast 'normal)
+
+  (defun solarized-light ()
+      (load-theme 'solarized t)
+      (set-frame-parameter nil 'background-mode 'light)
+      (enable-theme 'solarized))
+
+  (defun solarized-dark ()
+      (load-theme 'solarized t)
+      (set-frame-parameter nil 'background-mode 'dark)
+      (enable-theme 'solarized))
+
+  (defun solarized-switch ()
+      (interactive)
+      (if (string= (frame-parameter nil 'background-mode) 'light)
+          (solarized-dark)
+        (solarized-light)))
+
+  (solarized-light)
+  :bind* (("C-c <f6>" . #'solarized-switch)))
+
+(use-feature emacs
+  :init
+  (setq cursor-type 'box
+        cursor-in-non-selected-windows 'hollow
+        visible-cursor nil
+        x-stretch-cursor nil)
+  :config
+  (blink-cursor-mode -1))
+
+(use-feature paren
+  :init
+  (setq show-paren-style 'parenthesis
+        show-paren-when-point-in-periphery t
+        show-paren-when-point-inside-paren t
+        blink-matching-paren nil)
+  (show-paren-mode 1))
 
 (setq gnutls-verify-error (getenv "INSECURE")
   tls-checktrust gnutls-verify-error
@@ -118,16 +190,39 @@
 
 (setq ffap-machine-p-known 'reject)
 
-(setq-default display-line-numbers-width 3)
+(use-package magit
+  :bind (("C-x g" . #'magit-status)
+         ("C-x M-g" . #'magit-dispatch)
+         ("C-c M-g" . #'magit-file-dispatch)))
 
-(setq-default display-line-numbers-widen t)
+(setq-default display-line-numbers-width 2
+              display-line-numbers-widen t)
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; (defun switch-relative-absolute-linum ()
+;;   (interactive)
+;;   (if (string= (bound-and-true-p display-line-numbers) 'relative)
+;;       (display-line-numbers 'relative)
+;;     (display-line-numbers 1)))
+
+;; (global-set-key (kbd "C-c <f7>") 'switch-relative-absolute-linum)
 
 (setq sentence-end-double-space nil
       delete-trailing-lines nil
       require-final-newline t
       tabify-regexp "^\t* [ \t]+")
+
+(use-feature windmove
+  :demand t
+  :config
+  (windmove-default-keybindings)
+
+  (when (fboundp 'windmove-display-default-keybindings)
+    (windmove-display-default-keybindings))
+
+  (when (fboundp 'windmove-delete-default-keybindings)
+    (windmove-delete-default-keybindings)))
 
 (setq-default word-wrap t
               truncate-lines t
@@ -157,11 +252,59 @@
 
 (setq apropos-do-all t)
 
-(setq ring-bell-function #'ignore
-      visible-bell nil)
+(use-package emacs
+  :init
+  (setq frame-title-format '("%b %& GNU Emacs")
+        echo-keystrokes 1e-6
+        find-file-visit-truename t
+        vc-follow-symlinks t
+        find-file-suppress-same-file-warnings t
+        ring-bell-function #'ignore
+        visible-bell t
+        disabled-command-function nil
+        confirm-kill-emacs #'y-or-n-p)
+  (fset #'yes-or-no-p #'y-or-n-p))
+
+(use-feature ibuffer
+  :config
+  (setq ibuffer-expert t
+        ibuffer-use-other-window nil
+        ibuffer-show-empty-filter-groups nil
+        ibuffer-saved-filter-groups
+        '(("Main"
+           ("Directories" (mode . dired-mode))
+           ("Org" (mode . org-mode))
+           ("Programming" (mode . prog-mode))
+           ("Markdown" (mode . markdown-mode))
+           ("Emacs" (or
+                     (name . "^\\*Help\\*$")
+                     (name . "^\\*Custom.*")
+                     (name . "^\\*Org Agenda\\*$")
+                     (name . "^\\*info\\*$")
+                     (name . "^\\*scratch\\*$")
+                     (name . "^\\*Backtrace\\*$")
+                     (name . "^\\*Completion\\*$")
+                     (name . "^\\*straight-process\\*$")
+                     (name . "^\\*Messages\\*$"))))))
+  :hook
+  (ibuffer-mode . hl-line-mode)
+  (ibuffer-mode . (lambda ()
+                    (ibuffer-switch-to-saved-filter-groups "Main")))
+  :bind
+  (([remap list-buffers] . #'ibuffer)))
+
+(use-feature saveplace
+  :demand t
+  :init
+  (setq save-place-file (concat cache-dir "saveplace")
+        save-place-limit 100)
+  :config
+  (save-place-mode +1))
 
 (use-package gcmh
   :straight (:host gitlab :repo "koral/gcmh")
+  :demand t
+  :delight
   :init
   (gcmh-mode 1))
 
@@ -194,69 +337,121 @@
 (unless is-windows
   (setq selection-coding-system 'utf-8))
 
-(setq-default tab-width 4
-      tab-always-indent t
-      indent-tabs-mode nil)
+(use-feature emacs
+  :init
+  (setq-default tab-width 4
+                tab-always-indent t
+                indent-tabs-mode nil))
+
+(use-feature delsel
+  :demand t
+  :config
+  (delete-selection-mode +1))
 
 (setq kill-do-not-save-duplicates t)
 
 (setq mouse-yank-at-point t)
 
-(setq x-underline-at-descent-line t)
+(use-feature isearch
+  :config
+  (setq lazy-highlight-initial-delay 0))
 
-(straight-use-package 'doom-themes)
-(use-package emacs-color-theme-solarized
-  :straight (:host github :repo "sellout/emacs-color-theme-solarized")
+(use-feature emacs
   :init
-  (setq solarized-termcolor 256
-    solarized-broken-srgb t)
-  (load-theme 'solarized t)
-  (set-frame-parameter nil 'background-mode 'light)
-  (enable-theme 'solarized))
+  (setq auto-save-default nil
+        create-lockfiles nil
+        make-backup-files nil
+        auto-save-list-file-name           (concat cache-dir "autosave")
+        backup-directory-alist             `(("." . ,(concat cache-dir "backup/")))
+        abbrev-file-name                   (concat local-dir "abbrev.el")
+        async-byte-compile-log-file        (concat etc-dir "async-bytecomp.log")
+        bookmark-default-file              (concat etc-dir "bookmarks")
+        custom-file                        (concat local-dir "custom.el")
+        custom-theme-directory             (concat local-dir "themes/")
+        desktop-dirname                    (concat etc-dir "desktop")
+        desktop-base-file-name             "autosave"
+        desktop-base-lock-name             "autosave-lock"
+        pcache-directory                   (concat cache-dir "pcache/")
+        request-storage-directory          (concat cache-dir "request")
+        server-auth-dir                    (concat cache-dir "server/")
+        shared-game-score-directory        (concat etc-dir "shared-game-score/")
+        tramp-auto-save-directory          (concat cache-dir "tramp-auto-save/")
+        tramp-backup-directory-alist backup-directory-alist
+        tramp-persistency-file-name        (concat cache-dir "tramp-persistency.el")
+        url-cache-directory                (concat cache-dir "url/")
+        url-configuration-directory        (concat etc-dir "url/")
+        gamegrid-user-score-file-directory (concat etc-dir "games/")))
 
-(setq frame-title-format '("%b")
-  icon-title-format frame-title-format)
+(use-feature autorevert
+  :defer 2
+  :delight
+  :config
+  (setq auto-revert-interval 1)
+  (global-auto-revert-mode +1)
+  (setq global-auto-revert-non-file-buffers t)
+  (setq revert-without-query '(".*")))
 
-(setq use-dialog-box nil)
-(when (bound-and-true-p tooltip-mode)
-  (tooltip-mode -1))
-(when is-linux
-  (setq x-gtk-use-system-tooltips nil))
+(use-feature recentf
+  :demand t
+  :init
+  (setq recentf-save-file (concat cache-dir "recentf")
+        recentf-auto-cleanup 'never
+        recentf-max-menu-items 10
+        recentf-max-saved-items 100)
+  :config
+  (recentf-mode 1))
 
-(blink-cursor-mode -1)
-(setq blink-matching-paren nil
-      visible-cursor nil
-      x-stretch-cursor nil)
+(use-feature dired
+  :config
+  (setq dired-auto-revert-buffer t
+        dired-recursive-copies 'top
+        dired-recursive-deletes 'top
+        delete-by-moving-to-trash t
+        dired-listing-switches "-AFhv --color"
+        dired-dwim-target t)
+  :hook ((dired-mode . dired-hide-details-mode)
+         (dired-mode . hl-line-mode)))
 
-(setq find-file-visit-truename t
-      vc-follow-symlinks t)
+(use-feature dired-aux
+  :config
+  (setq dired-isearch-filenames 'dwim
+        dired-create-destination-dirs 'ask))
 
-(setq find-file-suppress-same-file-warnings t)
+(use-feature wdired
+  :after dired
+  :commands wdired-change-to-wdired-mode
+  :config
+  (setq wdired-allow-to-change-permissions t
+        wdired-create-parent-directories t)
 
-(setq delete-by-moving-to-trash is-mac)
-(setq delete-by-moving-to-trash is-windows)
+  (defun dired-back-to-start-of-files ()
+    (interactive)
+    (backward-char (- (current-column) 2)))
 
-(setq auto-save-default nil
-      create-lockfiles nil
-      make-backup-files nil
-      auto-save-list-file-name (concat cache-dir "autosave")
-      backup-directory-alist `(("." . ,(concat cache-dir "backup/"))))
+  :bind (:map wdired-mode-map
+              ([remap move-beginning-of-line] . #'dired-back-to-start-of-files)))
 
-(setq abbrev-file-name             (concat local-dir "abbrev.el")
-  async-byte-compile-log-file  (concat etc-dir "async-bytecomp.log")
-  bookmark-default-file        (concat etc-dir "bookmarks")
-  custom-file                  (concat local-dir "custom.el")
-  custom-theme-directory       (concat local-dir "themes/")
-  desktop-dirname              (concat etc-dir "desktop")
-  desktop-base-file-name       "autosave"
-  desktop-base-lock-name       "autosave-lock"
-  pcache-directory             (concat cache-dir "pcache/")
-  request-storage-directory    (concat cache-dir "request")
-  server-auth-dir              (concat cache-dir "server/")
-  shared-game-score-directory  (concat etc-dir "shared-game-score/")
-  tramp-auto-save-directory    (concat cache-dir "tramp-auto-save/")
-  tramp-backup-directory-alist backup-directory-alist
-  tramp-persistency-file-name  (concat cache-dir "tramp-persistency.el")
-  url-cache-directory          (concat cache-dir "url/")
-  url-configuration-directory  (concat etc-dir "url/")
-  gamegrid-user-score-file-directory (concat etc-dir "games/"))
+(use-feature dired-x
+  :after dired
+  :config
+  (when-let (cmd (cond (is-mac "open")
+                       (is-linux "xdg-open")
+                       (is-windows "start")))
+    (setq dired-guess-shell-alist-user
+          `(("\\.\\(?:docx\\|pdf\\|djvu\\|eps\\)\\'" ,cmd)
+            ("\\.\\(?:jpe?g\\|png\\|gif\\|xpm\\)\\'" ,cmd)
+            ("\\.\\(?:xcf\\)\\'" ,cmd)
+            ("\\.csv\\'" ,cmd)
+            ("\\.tex\\'" ,cmd)
+            ("\\.\\(?:mp4\\|mkv\\|avi\\|flv\\|rm\\|rmvb\\|ogv\\)\\(?:\\.part\\)?\\'" ,cmd)
+            ("\\.\\(?:mp3\\|flac\\)\\'" ,cmd)
+            ("\\.html?\\'" ,cmd)
+            ("\\.md\\'" ,cmd)))))
+
+(use-feature icomplete
+  :init
+  (fido-mode 1))
+
+(use-feature org-indent
+  :init
+  (add-hook 'org-mode-hook #'org-indent-mode))
